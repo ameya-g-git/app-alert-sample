d6d23dcebae1dd52a00fbd05afcabc905b03e800
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-11-10 14:54:06 -0500

    changed structure of request data

--- app/routes.py
+++ app/routes.py
@@ -5,12 +5,12 @@ import requests, socket
 
 main = Blueprint('main', __name__)
 
-@main.route("/<route>")
+@main.route("/<route>", methods=['GET', 'POST'])
 def send_request_data(route : str):
     request_data = {
-        "route": route,
         "ip": request.remote_addr,
-        "status": 200
+        "endpoint": route,
+        "method": request.method
     }
-    requests.post(url="http://127.0.0.1:4001/api/sample" ,json=request_data)
+    requests.post(url="http://127.0.0.1:4001/api/traffic" ,json=request_data)
     return jsonify(request_data)
